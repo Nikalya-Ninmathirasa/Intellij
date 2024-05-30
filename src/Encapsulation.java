8d5f9a0378405c7dc5a0aa18f3466ed3a251fde1
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-05-30 15:10:43 +0530

    "added more"

---
+++ src/Encapsulation.java
@@ -0,0 +1,39 @@
+public class Encapsulation {
+
+      String name;
+        int age;
+
+
+       void display(){
+            System.out.println(name);
+            System.out.println(age);
+        }
+
+
+     void setter(String n1, int num){
+            name = n1;
+            age=num;
+        }
+
+        String getter(){
+            return name;
+        }
+
+    }
+
+    class sample {
+
+        public static void main(String[] args) {
+
+            Encapsulation obj = new Encapsulation();
+            obj.setter("Joy", 20);
+            obj.display();
+            System.out.println(obj.getter());
+
+
+
+        }
+    }
+
+
+
